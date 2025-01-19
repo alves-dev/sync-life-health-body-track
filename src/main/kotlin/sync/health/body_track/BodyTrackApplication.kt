88da49d1763b65ac2eccf46779b85de4192f3888
Igor Alves <alvesmoreiraigor@gmail.com>
2025-01-19 10:03:33 -0300

    init project files

---
+++ src/main/kotlin/sync/health/body_track/BodyTrackApplication.kt
@@ -0,0 +1,11 @@
+package sync.health.body_track
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BodyTrackApplication
+
+fun main(args: Array<String>) {
+	runApplication<BodyTrackApplication>(*args)
+}
---
+++ src/test/kotlin/sync/health/body_track/BodyTrackApplicationTests.kt
@@ -0,0 +1,13 @@
+package sync.health.body_track
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BodyTrackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
